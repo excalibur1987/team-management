7fbaf36a1f0f9f9b73ec0ba8b8619cf14019781e
Omar <excalibur1987@gmail.com>
2021-05-25 17:14:25 +0300

    🎉 feat: add endpoint to change allowed entities for roles

--- app/apis/v1/roles/models/_Role.py
+++ app/apis/v1/roles/models/_Role.py
@@ -1,16 +1,36 @@
+from typing import TYPE_CHECKING
+
+from app.database import BaseModel, db
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql.schema import Column
 from sqlalchemy.sql.sqltypes import String
 
-from app.database import BaseModel
+if TYPE_CHECKING:
+    from ...entities.models import Entity
+    from ._RoleEntityPermission import RoleEntityPermission  # NOQA
 
 
 class Role(BaseModel):
     """contains basic roles for the aplication"""
 
     __tablename__ = "roles"
-    name = Column(String, nullable=False)
-    description = Column(String, nullable=False, server_default="")
+    name = Column(String, nullable=False, comment="role name")
+    description = Column(
+        String,
+        nullable=False,
+        server_default="",
+        comment="short discription of the role",
+    )
+
+    entity_permissions = relationship("RoleEntityPermission",)
 
     def __init__(self, name: str, description: str) -> None:
         self.name = name
         self.description = description
+
+    def add_entity(self, entity: "Entity", create: bool = False, edit: bool = False):
+        from ._RoleEntityPermission import RoleEntityPermission  # NOQA
+
+        permission = RoleEntityPermission(entity=entity, role=self, create=create, edit=edit)
+        db.session.add(permission)
+        db.session.commit()
--- app/apis/v1/roles/resources.py
+++ app/apis/v1/roles/resources.py
@@ -1,19 +1,30 @@
-from flask_jwt_extended import jwt_required
-from flask_restx import Resource, fields
-from flask_restx.reqparse import RequestParser
+from typing import List
 
 from app.database import db
 from app.exceptions import InvalidUsage
 from app.utils.decorators import has_roles
 from app.utils.extended_objects import ExtendedNameSpace
 from app.utils.helpers import argument_list_type
+from flask_jwt_extended import jwt_required
+from flask_restx import Resource, fields
+from flask_restx.errors import abort
+from flask_restx.reqparse import RequestParser
 
+from ..entities.models import Entity
 from ..users.models import User, UserRoles
 from ..users.resources import user_model
 from .models import Role
 
 api = ExtendedNameSpace("roles", description="Roles operations")
 
+role_entity_permission_model = api.model(
+    "RoleEntityPermission",
+    {
+        "name": fields.String(attribute="entity.name"),
+        "canCreate": fields.Boolean(attribute="can_create"),
+        "canEdit": fields.Boolean(attribute="can_edit"),
+    },
+)
 
 roles_model = api.model(
     "Role",
@@ -29,6 +40,10 @@ roles_model = api.model(
             .with_entities(User.id.label("id"), User.username.label("username"))
             .filter(UserRoles.role_id == role.id)
             .all(),
+        ),
+        "entities": fields.List(
+            fields.Nested(role_entity_permission_model, skip_none=True),
+            attribute="entity_permissions",
         ),
     },
 )
@@ -83,8 +98,10 @@ class RoleResource(Resource):
     @has_roles("admin")
     @api.marshal_with(roles_model)
     def get(self, role_id: int):
-
-        return Role.get(role_id)
+        role_ = Role.get(role_id)
+        if not role_:
+            raise abort(404)
+        return role_
 
     @jwt_required()
     @has_roles("admin")
@@ -92,6 +109,8 @@ class RoleResource(Resource):
     @api.expect(users_ids_model, validate=True)
     def post(self, role_id: int):
         role_ = Role.get(role_id)
+        if not role_:
+            raise abort(404)
 
         args = user_ids_parser.parse_args()
         if User.query.filter(User.id.in_(args.get("users"))).count() != len(
@@ -118,5 +137,57 @@ class RoleResource(Resource):
         return role
 
 
+class RoleEntityPermissionsResource(Resource):
+
+    parser = RequestParser()
+    parser.add_argument(
+        "entityId",
+        dest="entity_id",
+        type=int,
+        location="json",
+        required=True,
+        help="id of entity to add to role's permission",
+    )
+    parser.add_argument("canCreate", dest="can_create", type=bool, location="json")
+    parser.add_argument("canEdit", dest="can_edit", type=bool, location="json")
+
+    @jwt_required()
+    @has_roles("admin")
+    @api.expect(parser)
+    @api.marshal_with(roles_model)
+    def post(self, role_id: int):
+        role = Role.get(id=role_id)
+        if not role:
+            raise abort(404)
+        args = self.parser.parse_args()
+        entity = Entity.get(id=args.pop("entity_id"))
+        if not entity:
+            raise InvalidUsage.custom_error("invalid entity", 401)
+        role.add_entity(entity, **args)
+
+        return role
+
+    @jwt_required()
+    @has_roles("admin")
+    @api.expect(parser)
+    @api.marshal_with(roles_model)
+    def put(self, role_id: int):
+        role = Role.get(id=role_id)
+        if not role:
+            raise abort(404)
+        args = self.parser.parse_args()
+        entity_id = args.pop("entity_id")
+        entity: List[Entity] = [
+            ent
+            for ent in role.entity_permissions
+            if ent.entity_id == entity_id
+        ]
+        if len(entity) != 1:
+            raise InvalidUsage.custom_error("invalid entity", 401)
+        entity[0].update(ignore_none=True, **args)
+        return role
+
+
 api.add_resource(RolesResource, "/")
 api.add_resource(RoleResource, "/<role_id>")
+api.add_resource(RoleEntityPermissionsResource, "/<role_id>/entity")
