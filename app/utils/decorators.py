9b0e474e3810b19085e5a218ed773f223fcfee90
Omar <excalibur1987@gmail.com>
2021-05-25 17:16:05 +0300

    🎉 feat: add decorator to restrict access to resources based on user entity permissions

--- app/utils/decorators.py
+++ app/utils/decorators.py
@@ -1,10 +1,9 @@
 from functools import wraps
-from typing import Callable, Iterable, List, Union
-
-from flask import g
-from flask_principal import Identity, Permission, RoleNeed
+from typing import Callable, Iterable, List, Literal, Union
 
 from app.exceptions import InvalidUsage
+from flask import g
+from flask_principal import Identity, Need, Permission, RoleNeed
 
 
 def check_roles(
@@ -39,6 +38,24 @@ def has_roles(*args: Iterable[str]):
         def wrapped(*args, **kwargs):
             identity: Identity = g.identity
             if not check_roles(identity=identity, roles=roles):
+                raise InvalidUsage.user_not_authorized()
+            return fn(*args, **kwargs)
+
+        return wrapped
+
+    return wrapper
+
+
+def has_permission(entity: str, permissions: List[Literal['create', 'edit']] = ['create']):
+    """checks if user has required permissions for this entity"""
+
+    permissions = [Permission(Need(entity, perm)) for perm in permissions]
+
+    def wrapper(fn: Callable):
+        @wraps(fn)
+        def wrapped(*args, **kwargs):
+            identity: Identity = g.identity
+            if False in [identity.can(perm) for perm in permissions]:
                 raise InvalidUsage.user_not_authorized()
             return fn(*args, **kwargs)
 
